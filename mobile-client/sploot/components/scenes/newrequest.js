7c223b570167026dc96b436c785778d9a3b779f0
kaveesha <shehanhere@gmail.com>
2019-08-15 07:39:57 +0530

    gepa - fetch from backend

--- mobile-client/sploot/components/scenes/newrequest.js
+++ mobile-client/sploot/components/scenes/newrequest.js
@@ -38,7 +38,24 @@ export default class NewRequest extends Component{
             latitudeDelta: 0.00922 * 1.5,
             longitudeDelta: 0.00421 * 1.5,
             selectedAnimal: "",
-            uploading: false,
+			uploading: false,
+			title: "", //send
+            track: true, //send
+            contact: "", //send
+            medical: false, //send
+            lost: false, //send
+            tresspassable: false, //send
+            aggression: false, //send
+            additional: "", //send
+            step: 0,
+            tracker: [], //send
+            location: "",
+            loading: false,
+            pictures: [], //send
+            timestamp: "", //ok
+            picurl:[],
+			mapLocation: [7.8731, 80.7718],
+			milestones:{id:"0000"}
         }
 		this.selectPhotoTapped = this.selectPhotoTapped.bind(this);
 		this.mapRef = null;
@@ -104,16 +121,31 @@ export default class NewRequest extends Component{
 	}
 
 	submit = () => {
-        // if (this.state.selectedAnimal == "") {
-        //     this.dropdown.alertWithType("error", "Error", "Please Select An Animal");
-        // } else if (this.state.description == "") {
-        //     this.dropdown.alertWithType("error", "Error", "Please Add The Description");
-        // } else {
-        //     this.uploadImage();
+	  console.log(this.state)
+	  let payload = new FormData();
+	  payload.append('title',this.state.title);
+	  payload.append('description',this.state.description);
+	  payload.append('tracker',this.state.track);
+	  payload.append('milestones',this.state.milestones);
 
-        // }
+	  fetch('http://192.168.8.103:3000/api/requests/new',{
+		  method:"POST",
+		  body:payload
+	  })
+	  .then(
+		  res=>console.log(res)
+	  )
     }
 
+	handleChange(e) {
+		console.log(this.state)
+		var key = e.target.name;
+		var value = e.target.value;
+        this.setState({
+            [key]: value,
+
+        })
+    }
 
 	render(){
 		return(
@@ -131,7 +163,7 @@ export default class NewRequest extends Component{
 			
 				<Image
 				style={styles.avatar}
-				source={require('../images/image_upload_avatar.png')}/> 
+				source={require('../images/sploot.png')}/> 
 				// <Text>"sgdfhgdhs"</Text>
 				
 				) : (
@@ -143,6 +175,9 @@ export default class NewRequest extends Component{
 			<TextInput
 			style={[styles.textInputStyle]}
 			placeholder={"Title"}
+			value={this.state.title}
+			onChangeText={(title) => {
+				this.setState({title},()=>(console.log(this.state)))}}
 			/>
 
 			<TextInput
@@ -150,6 +185,9 @@ export default class NewRequest extends Component{
 			placeholder={"Enter Description Here"}
 			multiline = {true}
 			numberOfLines = {4}
+			value = {this.state.additional}
+			onChangeText={(additional) => {
+				this.setState({additional},()=>(console.log(this.state)))}}
 			/>
 			 
 			 <MapView
@@ -206,7 +244,7 @@ const styles = StyleSheet.create({
 	  justifyContent: 'flex-start',
 	  alignItems: 'center',
 	  backgroundColor: '#6997e0',
-	  borderRadius: 50,
+	  borderRadius: 100,
 	  marginTop: 15,
 	},
 
@@ -256,9 +294,9 @@ const styles = StyleSheet.create({
 	
 	avatar: {
 
-	  borderRadius: 20,
-	  width: 100,
-	  height: 100,
+	  borderRadius: 100,
+	  width: 200,
+	  height: 200,
 	},
 
 	button1:{
